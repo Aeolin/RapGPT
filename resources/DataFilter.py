26481a29f168b0c7e790b2c072a4b84e69d37f09
AdiJoost <adi.joost@gmail.com>
2024-04-12 08:57:54 +0200

    added new folder and some minor refactoring

--- resources/DataFilter.py
+++ resources/DataFilter.py
@@ -6,6 +6,8 @@ import pandas as pd
 GIT_REPO_NAME = "RapGPT"
 DATA_FOLDER = "resources"
 GENRES_FOLDER = "genres"
+NO_GENRE = "NULL"
+MANY_GENRES = "MANY"
 
 noMatchesTags = []
 
@@ -13,48 +15,28 @@ def main():
     filterSongBatch(None, "song_batch_3.json")
 
 def filterSongBatch(tags, filename, genresFileName="expandedChatGPT.json"):
-    #get path
     path = getPathToFile(filename)
     genres = getGenres(genresFileName)
-    #load file
     data = loadJson(path)
+    textGenresPair = [generateTextGenrePair(dataPoint, genres) for dataPoint in data]
+    saveJson()
 
-    noMatches = 0
-    oneMatch = 0
-    manyMatches = 0
-    for dataPoint in data:
-        textGenrePair = generateTextGenrePair(dataPoint, genres)
-        if textGenrePair["Genres"] == "Null":
-            noMatches += 1
-        elif textGenrePair["Genres"] == "Many":
-            manyMatches += 1
-        else:
-            oneMatch += 1
-    print(f"One Matches: {oneMatch}: {100* float(oneMatch) / len(data)}%")
-    print(f"No Matches: {noMatches}: {100* float(noMatches) / len(data)}%")
-    print(f"Many Matches: {manyMatches}: {100* float(manyMatches) / len(data)}%")
-    #countOthers()
-
-def countOthers():
-    df = pd.DataFrame({"Tag": noMatchesTags})
-    print(df.value_counts())
-
+def saveJson():
+    pass
 
 def generateTextGenrePair(dataPoint, genres: set, selectAtRandom = True):
-    #validate dataPoint?
-    #intersect
+    """selectAtRandom: if song has many genres, selects one at random and dismisses the others."""
     intersection = genres.intersection(dataPoint["LastFMTags"])
-    #if intersect, return text + genre
     if len(intersection) < 1:
         for tag in dataPoint["LastFMTags"]:
             noMatchesTags.append(tag.lower())
-        return {"Lyrics": dataPoint["Lyrics"], "Genres": "Null"}
+        return {"Lyrics": dataPoint["Lyrics"], "Genres": NO_GENRE, "GeniusId": dataPoint["GeniusId"]}
     if len(intersection) == 1:
-        return {"Lyrics": dataPoint["Lyrics"], "Genres": list(intersection)[0]}
+        return {"Lyrics": dataPoint["Lyrics"], "Genres": list(intersection)[0], "GeniusId": dataPoint["GeniusId"]}
     if selectAtRandom:
         randomIndex = random.randint(0, len(intersection) - 1)
-        return {"Lyrics": dataPoint["Lyrics"], "Genres": list(intersection)[randomIndex]}
-    return {"Lyrics": dataPoint["Lyrics"], "Genres": "Many"}
+        return {"Lyrics": dataPoint["Lyrics"], "Genres": list(intersection)[randomIndex], "GeniusId": dataPoint["GeniusId"]}
+    return {"Lyrics": dataPoint["Lyrics"], "Genres": MANY_GENRES}
     
 
 def getPathToFile(filename):
@@ -78,9 +60,6 @@ def getPathToGenresFile(filename):
     if len(head) < 1:
         raise NameError("Cannot get the working Path. Look at Source Code and Debug. :/")
     return os.path.join(head[0], GIT_REPO_NAME, DATA_FOLDER, GENRES_FOLDER, filename)
-
-
-
 
 def loadJson(path):
     if not os.path.isfile(path):
